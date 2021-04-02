27663e2162aeb1292292595b4fdc43b8c24029f9
DYGV <11eisuke88@gmail.com>
2021-04-02 12:00:02 +0900

    refine

--- source/app.d
+++ source/app.d
@@ -3,35 +3,41 @@ import std.range: empty;
 import commandr;
 import repo;
 
-void main(string[] args)
-{
-    auto a = new Program("small-git", "1.0")
+void main(string[] args){
+    Program program = new Program("small-git", "1.0")
           .summary("Write-Yourself-a-git implemented in D")
-          .author("DYGV")
-          .add(new Command("init")
-              .add(new Argument("dir", "directory").defaultValue("./")))
-          .add(new Command("cat-file")
+          .author("DYGV");
+
+    // add "init" command
+    program.add(new Command("init")
+              .add(new Argument("dir", "directory").defaultValue("./")));
+
+    // add "cat-file"
+    program.add(new Command("cat-file")
               .add(new Flag("t", null, "show object type")
                 .name("type"))
               .add(new Flag("s", null, "show object type")
                 .name("size"))
               .add(new Flag("p", null, "pretty-print object's content")
                 .name("pprint"))
-            .add(new Argument("object", "object id"))
-          )
-          .parse(args);
+            .add(new Argument("object", "object id")));
 
-  a.on("init", (args) {
-      writeln("arg: ", args.arg("dir"));
-      cmd_init(args.arg("dir"));
-  });
+    auto a = program.parse(args);
 
-  a.on("cat-file", (args) {
-      writeln("t: ", args.flag("type"));
-      writeln("s: ", args.flag("size"));
-      writeln("p: ", args.flag("pprint"));
-      writeln("object: ", args.arg("object"));
-  });
+    a.on("init", (args){
+        cmd_init(args.arg("dir"));
+    });
+
+    a.on("cat-file", (args){
+        bool type = args.flag("type"),
+             size = args.flag("size"),
+             pprint = args.flag("pprint");
+        if(!(type|size|pprint)){
+            program.commands["cat-file"].printHelp();
+            return;
+        }
+        string object = args.arg("object");
+    });
 }
 
 void cmd_add(string args){
